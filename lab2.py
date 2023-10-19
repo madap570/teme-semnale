f6587f41bc47ab6f8fe95e76b9edafab8f3c7459
madap570 <madanico02@gmail.com>
2023-10-19 18:54:02 +0300

    laborator 2 - exercitiul 1

--- lab2.py
+++ lab2.py
@@ -34,39 +34,4 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-#ex 4
-amplitudine_sinus = 1.0
-frecventa_sinus = 5.0
-amplitudine_sawtooth = 0.5
-frecventa_sawtooth = 2.0
-n = np.arange(0, 1000)
-
-semnal_sinus = amplitudine_sinus * np.sin(2 * np.pi * frecventa_sinus * n)
-
-semnal_sawtooth = amplitudine_sawtooth * signal.sawtooth(2 * np.pi * frecventa_sawtooth * n)
-
-suma_semnalelor = semnal_sinus + semnal_sawtooth
-
-plt.figure(figsize=(12, 8))
-
-plt.subplot(3, 1, 1)
-plt.plot(n, semnal_sinus)
-plt.title('Semnal Sinusoidal')
-plt.xlabel('Eșantionare')
-plt.ylabel('Amplitudine')
-
-plt.subplot(3, 1, 2)
-plt.plot(n, semnal_sawtooth)
-plt.title('Semnal de Tip Sawtooth')
-plt.xlabel('Eșantionare')
-plt.ylabel('Amplitudine')
-
-plt.subplot(3, 1, 3)
-plt.plot(n, suma_semnalelor)
-plt.title('Suma Semnalelor')
-plt.xlabel('Eșantionare')
-plt.ylabel('Amplitudine')
-
-plt.tight_layout()
-plt.show()
 
